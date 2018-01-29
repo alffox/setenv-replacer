6ecd1f63bb24ccbc9a593c40724d23a305ed806c
Alfonso Crisci <30409634+alffox@users.noreply.github.com>
2018-01-29 15:00:50 +0100

    bug: Use correct replaced value

--- setenv-replacer.sh
+++ setenv-replacer.sh
@@ -5,7 +5,7 @@ Orange='\033[0;33m'
 Green='\033[0;32m'
 Red='\033[0;31m'
 
-echo -e "${Orange}Replacing -Xmx1024m -XX:MaxPermSize=384m with -Xmx1024m in your setenv.sh file, please wait ..."
+echo -e "${Orange}Replacing -Xmx1024m -XX:MaxPermSize=384m with -Xmx2048m in your setenv.sh file, please wait ..."
 
 sed -i 's/ -Xmx1024m -XX:MaxPermSize=384m/-Xmx2048m/g' tomcat-8.0.32/bin/setenv.sh
 
